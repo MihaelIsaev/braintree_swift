8f7740c24f766b74238b4fc29779847039494b28
Mihael Isaev <isaev.mihael@gmail.com>
2018-09-29 18:07:02 +0300

    Add XMLParsing lib

--- Package.swift
+++ Package.swift
@@ -14,13 +14,14 @@ let package = Package(
     dependencies: [
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+        .package(url: "https://github.com/MihaelIsaev/XMLParsing.git", .branch("master"))
         ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Braintree",
-            dependencies: ["Vapor"]),
+            dependencies: ["Vapor", "XMLParsing"]),
         .testTarget(
             name: "BraintreeTests",
             dependencies: ["Braintree"]),

70e9f589b1e404aa6c80d261ee5af044e0f07957
Mihael Isaev <isaev.mihael@gmail.com>
2018-09-29 16:00:51 +0300

    CreditCardAddressRequest: implement public initializer

--- Sources/Braintree/Requests/CreditCardAddressRequest.swift
+++ Sources/Braintree/Requests/CreditCardAddressRequest.swift
@@ -11,6 +11,10 @@ import Vapor
 public final class CreditCardAddressRequest: Content {
     public var updateExisting: Bool?
     
+    public init (updateExisting: Bool? = nil) {
+        self.updateExisting = updateExisting
+    }
+    
     private enum CodingKeys : String, CodingKey {
         case updateExisting = "update-existing"
     }
